f8e7c07641db89926973b6c76d35b6eadb849567
lenonleite <lenonleite@gmail.com>
2024-09-24 16:02:39 +0100

    - add add item in construct to adapt in extended CompanyModel - change test

--- LeuchtfeuerCompanyPointsBundle.php
+++ LeuchtfeuerCompanyPointsBundle.php
@@ -9,15 +9,4 @@ use MauticPlugin\LeuchtfeuerCompanyPointsBundle\Helper\InstallHelper;
 
 class LeuchtfeuerCompanyPointsBundle extends AbstractPluginBundle
 {
-    public const FIELD_DATA = [
-        'alias' => 'score_calculated',
-        'name'  => 'Score Calculated',
-        'type'  => 'number',
-    ];
-
-    public static function onPluginInstall(Plugin $plugin, MauticFactory $factory, $metadata = null, $installedSchema = null): void
-    {
-        InstallHelper::installField($factory, (string) $factory->getParameter('mautic.db_table_prefix'), self::FIELD_DATA['alias'], self::FIELD_DATA['name'], self::FIELD_DATA['type']);
-        parent::onPluginInstall($plugin, $factory, $metadata, $installedSchema);
-    }
 }
