479ef1c59c9f839df3cbd4acec29d4dac12e55f4
lenonleite <lenonleite@gmail.com>
2024-09-24 16:05:52 +0100

    - add add item in construct to adapt in extended CompanyModel - change test

--- LeuchtfeuerCompanyPointsBundle.php
+++ LeuchtfeuerCompanyPointsBundle.php
@@ -2,10 +2,7 @@
 
 namespace MauticPlugin\LeuchtfeuerCompanyPointsBundle;
 
-use Mautic\CoreBundle\Factory\MauticFactory;
 use Mautic\IntegrationsBundle\Bundle\AbstractPluginBundle;
-use Mautic\PluginBundle\Entity\Plugin;
-use MauticPlugin\LeuchtfeuerCompanyPointsBundle\Helper\InstallHelper;
 
 class LeuchtfeuerCompanyPointsBundle extends AbstractPluginBundle
 {
