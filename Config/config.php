5e745fed6bbdf896c003c8953f64ed606d0f30ba
Ekkehard Gümbel <43146234+ekkeguembel@users.noreply.github.com>
2024-12-27 14:26:49 +0100

    fix version number in config.php

--- Config/config.php
+++ Config/config.php
@@ -3,7 +3,7 @@
 return [
     'name'        => 'Company Points by Leuchtfeuer',
     'description' => 'Massively enhanced Company-based Scoring',
-    'version'     => '1.0.0',
+    'version'     => '1.1.1',
     'author'      => 'Leuchtfeuer Digital Marketing GmbH',
     'routes'      => [
         'main' => [
