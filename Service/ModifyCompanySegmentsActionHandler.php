60988f6a46cb4b5dc44f0dfa195964e170da11d6
Jonas <j.ludwig@leuchtfeuer.com>
2026-02-04 11:37:11 +0100

    MTC-9771 QA Findings

--- Service/ModifyCompanySegmentsActionHandler.php
+++ Service/ModifyCompanySegmentsActionHandler.php
@@ -3,40 +3,44 @@
 namespace MauticPlugin\LeuchtfeuerCompanyPointsBundle\Service;
 
 use Mautic\LeadBundle\Entity\Company;
+use MauticPlugin\LeuchtfeuerCompanySegmentsBundle\Entity\CompanySegmentRepository;
 use MauticPlugin\LeuchtfeuerCompanySegmentsBundle\Model\CompanySegmentModel;
 
 class ModifyCompanySegmentsActionHandler
 {
     public function __construct(
+        private CompanySegmentRepository $companySegmentRepository,
         private CompanySegmentModel $companySegmentModel,
     ) {
     }
+
     /**
-     * @param Company              $company
      * @param array<string, mixed> $triggerProperties
      */
     public function execute(Company $company, array $triggerProperties): void
     {
+        $segmentsToAdd            = [];
+        $segmentsToRemove         = [];
         $segmentIdsToAdd          = $triggerProperties['add_segments'] ?? [];
         $segmentIdsToRemove       = $triggerProperties['remove_segments'] ?? [];
 
         if (is_array($segmentIdsToAdd) && !empty($segmentIdsToAdd)) {
             /** @var array<int> $segmentIdsToAddTyped */
             $segmentIdsToAddTyped = array_map('intval', $segmentIdsToAdd);
-            $segmentsToAdd = $this->companySegmentModel->getRepository()->getSegmentObjectsViaListOfIDs($segmentIdsToAddTyped);
+            $segmentsToAdd        = $this->companySegmentRepository->getSegmentObjectsViaListOfIDs($segmentIdsToAddTyped);
         }
 
         if (is_array($segmentIdsToRemove) && !empty($segmentIdsToRemove)) {
             /** @var array<int> $segmentIdsToRemoveTyped */
             $segmentIdsToRemoveTyped = array_map('intval', $segmentIdsToRemove);
-            $segmentsToRemove = $this->companySegmentModel->getRepository()->getSegmentObjectsViaListOfIDs($segmentIdsToRemoveTyped);
+            $segmentsToRemove        = $this->companySegmentRepository->getSegmentObjectsViaListOfIDs($segmentIdsToRemoveTyped);
         }
 
-        if (!empty($segmentsToAdd)){
+        if ([] !== $segmentsToAdd) {
             $this->companySegmentModel->addCompany($company, $segmentsToAdd);
         }
 
-        if(!empty($segmentsToRemove)) {
+        if ([] !== $segmentsToRemove) {
             $this->companySegmentModel->removeCompany($company, $segmentsToRemove);
         }
     }
